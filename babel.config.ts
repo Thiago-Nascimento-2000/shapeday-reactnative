dbc9d9b6926f8b911ca1d90bf414acf7a6a5bb0e
Thiago de Oliveira Nascimento <veks@MacBook-Air-de-Thiago.local>
2026-06-05 22:00:41 -0300

    first commit

---
+++ babel.config.ts
@@ -0,0 +1,10 @@
+export default function (api: any) {
+  api.cache(true);
+
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
+}
